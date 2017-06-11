1bc85dfbdf347d6af7e64955b3f840b0fd856f5d
Michael Oswald <don.miguel@gmx.ch>
2017-06-11 10:32:02 +0200

    startu script loader

--- loader/loader.sh
+++ loader/loader.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 export CLASSPATH="../lib/hazelcast-3.8.2.jar:../lib/hazelcast-enterprise-3.8.2.jar:../lib/hazelcast-enterprise-client-3.8.2.jar:../lib/joda-time-2.7.jar:../lib/cache-api-1.0.0.jar:."
-cd ../common
+cd ../common/
+rm -r target 2>/dev/null
 find . -name "*java" -type f > files.txt
+mkdir target
+javac  -cp $CLASSPATH  @files.txt -d target
+export CLASSPATH="$CLASSPATH:../common/target/"
 
-
-
+cd ../loader
+rm -r target 2>/dev/null
 find . -name "*java" -type f > files.txt
-export CLASSPATH="../out/production/common:../lib/hazelcast-3.8.2.jar:../lib/hazelcast-enterprise-3.8.2.jar:../lib/hazelcast-enterprise-client-3.8.2.jar:../lib/joda-time-2.7.jar:../lib/cache-api-1.0.0.jar:."
+mkdir target
 javac  -cp $CLASSPATH  @files.txt -d target
 cp -r src/main/resources/* target/
 java -cp $CLASSPATH:target com.hazelcast.certification.process.LoaderMain
