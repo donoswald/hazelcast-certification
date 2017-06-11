40e2e28eec2acccc71d1c5cd5461586195c9ca6a
Michael Oswald <don.miguel@gmx.ch>
2017-06-11 10:18:37 +0200

    deleted

--- loader/loader.sh
+++ loader/loader.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+export CLASSPATH="../lib/hazelcast-3.8.2.jar:../lib/hazelcast-enterprise-3.8.2.jar:../lib/hazelcast-enterprise-client-3.8.2.jar:../lib/joda-time-2.7.jar:../lib/cache-api-1.0.0.jar:."
+cd ../common
+find . -name "*java" -type f > files.txt
+
+
+
 find . -name "*java" -type f > files.txt
 export CLASSPATH="../out/production/common:../lib/hazelcast-3.8.2.jar:../lib/hazelcast-enterprise-3.8.2.jar:../lib/hazelcast-enterprise-client-3.8.2.jar:../lib/joda-time-2.7.jar:../lib/cache-api-1.0.0.jar:."
 javac  -cp $CLASSPATH  @files.txt -d target
